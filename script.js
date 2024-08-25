5e7b8f5e649a9c3307834accffc081589f5dd3b1
kidaholy <kidayos2014@gmail.com>
2024-08-26 02:22:48 +0300

    Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var buttonColor = ["red", "blue", "green", "yellow"];
 function nextSequence () {
   var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColor(randomNumber);
 }
