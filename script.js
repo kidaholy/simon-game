f9dfe9d06fca8bc014fd9aa9f7282a152c3b3f40
kidaholy <kidayos2014@gmail.com>
2024-08-26 02:14:27 +0300

    create new function called gamePattern

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-var buttonColors = ["red", "blue", "green", "yellow"];
-var gamePattern = [];
-function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColor = buttonColors[randomNumber];
-  gamePattern.push(randomChosenColor);
+function nextSequence () {
+  
 }
