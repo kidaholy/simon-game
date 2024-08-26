d3252f815e787b7d17ec9b8d0988678e2a74319a
kidaholy <kidayos2014@gmail.com>
2024-08-26 12:41:10 +0300

    In the styles.css file, there is a class called 'game-over', apply this class to the body of the website when the user gets one of the answers wrong and then remove it after 200 milliseconds

--- script.js
+++ script.js
@@ -32,6 +32,11 @@ function checkAnswer (currentLevel) {
     } else {
       console.log("wrong");
       playSound("wrong");
+
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
     }
   }
 }
